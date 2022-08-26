371566cc6836450f44183090ef51c4afde5e217b
Parth Parmar <pp073522@gmail.com>
2022-08-26 15:18:30 +0530

    minor fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
@@ -8,7 +7,10 @@ export default function Home() {
 
   return (
     <div className="container mt-5 pt-5">
-      <h4>Home page is under construction</h4>
+      <h5>Home page is under construction</h5>
+      <h6><Link to='category/all'>Click here</Link> to view all products</h6>
     </div>
+
+    
   )
 }
